d6d1caba2fcd22dafab7066a35b04b9e18e1014b
محمد <mrg2233@g.coppellisd.com>
2025-03-07 18:57:44 -0600

    Update rng.py

--- rng.py
+++ rng.py
@@ -5,12 +5,12 @@ guessed = False
 guessc = 0
 
 while not guessed:
-    guess = int(input("Guess the number: "))
+    guess = input("Guess the number:")
     guessc = guessc + 1
-    if guess == i:
+    if int(guess) == i:
         guessed = True
         print(f'You did it in {guessc} tries!')
-    elif guess > i:
-        print("The number is less than", + guess)
-    elif guess < i:
-        print("The number is greater than", + guess)
+    elif int(guess) > i:
+        print(f'The number is less than {guess}.')
+    elif int(guess) < i:
+        print(f'The number is greater than {guess}.')
